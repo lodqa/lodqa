43b02e172709621088068e78b9dd73f38fb23d14
Satohi Tahara <tahara@digital-squad.net>
2013-06-28 15:35:30 +0900

    added simplecov gem and .gitignore

--- Gemfile
+++ Gemfile
@@ -16,4 +16,5 @@ group :test do
 	gem 'rack-test'
 	gem 'rspec'
 	gem 'factory_girl', "~> 4.0"
+	gem 'simplecov'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'rspec'
 require 'rack/test'
+require 'simplecov'
 
 include Rack::Test::Methods
+SimpleCov.start
 
 RSpec.configure do |config|
   # Use color in STDOUT
