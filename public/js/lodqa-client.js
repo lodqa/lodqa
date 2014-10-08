660827a238e71e1ad14534af41897e1ef2cf09da
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2014-10-08 14:33:22 +0900

    ノードにidを指定する必要がありました。

--- public/js/lodqa-client.js
+++ public/js/lodqa-client.js
@@ -20,9 +20,9 @@ window.onload = function() {
 
         var jsondata = JSON.parse(m.data);
         if ("anchored_pgp" in jsondata) {
-          emitter.emit('anchored_pgp', jsondata);
+          emitter.emit('anchored_pgp', jsondata.anchored_pgp);
         } else if ("solution" in jsondata) {
-          emitter.emit('solution', jsondata);
+          emitter.emit('solution', jsondata.solution);
         } else {
           show('websocket message: ' + m.data);
         }
@@ -34,15 +34,15 @@ window.onload = function() {
       var currentRegion;
 
       solution
-        .on('anchored_pgp', function(data) {
+        .on('anchored_pgp', function(anchored_pgp) {
           currentRegion = document.createElement('div');
           currentRegion.classList.add('section');
           currentRegion.style.border = "solid black 1px";
           document.getElementById('lodqa-results').appendChild(currentRegion);
-          currentRegion.innerHTML = JSON.stringify(data.anchored_pgp.nodes);
+          currentRegion.innerHTML = JSON.stringify(anchored_pgp.nodes);
         })
-        .on('solution', function(data) {
-          currentRegion.innerHTML += '<br />' + JSON.stringify(data.solution);
+        .on('solution', function(solution) {
+          currentRegion.innerHTML += '<br />' + JSON.stringify(solution);
         });
     },
     initGraph = function() {
@@ -54,26 +54,38 @@ window.onload = function() {
       });
 
       return graph;
+    },
+    bindGpaph = function(solution) {
+      solution
+        .on('anchored_pgp', function(anchored_pgp) {
+          var graph = initGraph();
+          Object.keys(anchored_pgp.nodes)
+            .map(function(key) {
+              return {
+                id: key,
+                label: anchored_pgp.nodes[key].term
+              };
+            })
+            .map(function(term) {
+              var path = decomposeUrl(term.label).path;
+              return {
+                id: term.id,
+                label: path[path.length - 1]
+              };
+            })
+            .forEach(function(term) {
+              graph.addNode(new Springy.Node(term.id, term));
+            });
+
+          console.log(graph);
+        })
+        .on('solution', function(solution) {
+          //   console.log(solution);
+        });
     };
 
   var solution = loadSolution();
 
-  solution.on('anchored_pgp', function(data) {
-    var graph = initGraph();
-    Object.keys(data.anchored_pgp.nodes)
-      .map(function(key) {
-        return data.anchored_pgp.nodes[key].term;
-      })
-      .map(function(term) {
-        var path = decomposeUrl(term).path;
-        return path[path.length - 1];
-      })
-      .forEach(function(term) {
-        graph.newNode({
-          label: term
-        });
-      });
-  })
-
   bindDebug(solution);
+  bindGpaph(solution);
 }
