5ed3af5e59b110cf29b3583b8fb1cd1159044e84
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2014-10-17 20:14:03 +0900

    stub用のsolution取得オブジェクトを追加します。

--- public/js/lodqa-client.js
+++ public/js/lodqa-client.js
@@ -30,6 +30,159 @@ window.onload = function() {
 
       return emitter;
     },
+    loadStubSolution = function() {
+      var emitter = new events.EventEmitter();
+      _.delay(function() {
+        emitter.emit('anchored_pgp', {
+          "nodes": {
+            "t1": {
+              "head": 2,
+              "text": "side effects",
+              "term": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/side_effects"
+            },
+            "t2": {
+              "head": 6,
+              "text": "streptomycin",
+              "term": "http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugs/DB01082"
+            }
+          },
+          "edges": [{
+            "subject": "t1",
+            "object": "t2",
+            "text": "associated with"
+          }],
+          "focus": "t1"
+        });
+      }, 100);
+      [{
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0002878",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0002994",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0011053",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0040034",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0041296",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0002418",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0002792",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0004610",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0007947",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0011606",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0002878",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0002994",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0011053",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0040034",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0041296",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0002418",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0002792",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0004610",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0007947",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }, {
+        "it1": "http://www4.wiwiss.fu-berlin.de/sider/resource/side_effects/C0011606",
+        "st1": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+        "x01": "http://www4.wiwiss.fu-berlin.de/sider/resource/drugs/5297",
+        "p01": "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect",
+        "p02": "http://www.w3.org/2002/07/owl#sameAs"
+      }]
+      .forEach(function(solution, index) {
+        _.delay(function() {
+          emitter.emit('solution', solution);
+        }, 100 * index + 100);
+
+      });
+      return emitter;
+    },
     bindDebug = function(solution) {
       var currentRegion;
 
