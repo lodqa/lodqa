044317dcee3c07405e2ccefbf284ecb3b92c1ae8
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2014-10-21 18:41:28 +0900

    Hide the json for debug.

--- public/js/lodqa-client.js
+++ public/js/lodqa-client.js
@@ -13,5 +13,5 @@ window.onload = function() {
 
   bindSolution(solution, lodqaClient.tablePresentation);
   bindSolution(solution, lodqaClient.graphPresentation);
-  bindSolution(solution, lodqaClient.debugPresentation);
+  // bindSolution(solution, lodqaClient.debugPresentation);
 }
