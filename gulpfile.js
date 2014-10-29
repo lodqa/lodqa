4d1e63c4ae591fba96802285a8e9593d51e3c986
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2014-10-29 17:11:50 +0900

    Watch all files in the src directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp'),
-  src_js = 'src/js/lodqa-client.js'
+  src = 'src/js/',
+  js = 'lodqa-client.js'
 
 gulp
   .task('browserify', function() {
     var browserify = require('gulp-browserify')
 
-    gulp.src(src_js)
+    gulp.src(src + js)
       .pipe(browserify())
       .pipe(gulp.dest('public/js/'))
   })
   .task('auto_compile', function() {
-    gulp.watch(src_js, ['browserify'])
+    gulp.watch(src + '**', ['browserify'])
   })
   .task('default', ['browserify', 'auto_compile'])
