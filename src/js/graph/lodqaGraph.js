13a3a0eef6d9dc610e188e5392e8972f3ee504f9
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2014-12-08 22:45:52 +0900

    Do not set positions in the pgp graph.

--- src/js/graph/lodqaGraph.js
+++ src/js/graph/lodqaGraph.js
@@ -38,90 +38,9 @@ var _ = require('lodash'),
   setFocus = function(focus, term) {
     return term.id === focus ? toFocus(term) : term;
   },
-  extendIndex = function(a, index) {
-    a.index = index;
-    return a;
-  },
-  threeNodeOrders = {
-    t1: [1, 0, 2],
-    t2: [0, 1, 2],
-    t3: [0, 2, 1]
-  },
-  getNodeOrder = function(id) {
-    return threeNodeOrders[id];
-  },
-  getTwoEdgeNode = function(edgeCount) {
-    return _.first(Object.keys(edgeCount)
-      .map(function(id) {
-        return {
-          id: id,
-          count: edgeCount[id]
-        };
-      })
-      .filter(function(node) {
-        return node.count === 2;
-      })
-      .map(function(node) {
-        return node.id;
-      }));
-  },
-  countEdge = function(edges) {
-    return edges.reduce(function(edgeCount, edge) {
-      edgeCount[edge.subject] ++;
-      edgeCount[edge.object] ++;
-      return edgeCount;
-    }, {
-      t1: 0,
-      t2: 0,
-      t3: 0
-    });
-  },
-  getOrderWhenThreeNode = _.compose(getNodeOrder, getTwoEdgeNode, countEdge),
-  specialSort = function(nodeOrder, a, b) {
-    return nodeOrder[a.index] - nodeOrder[b.index];
-  },
-  simpleSort = function(a, b) {
-    return b.index - a.index;
-  },
-  anchoredPgpNodePositions = [
-    [],
-    [{
-      x: 0,
-      y: 0
-    }],
-    [{
-      x: -20,
-      y: 20
-    }, {
-      x: 20,
-      y: -20
-    }],
-    [{
-      x: -40,
-      y: 40
-    }, {
-      x: 0,
-      y: 0
-    }, {
-      x: 40,
-      y: -40
-    }]
-  ],
-  setPosition = function(number_of_nodes, term, index) {
-    return _.extend(term, {
-      position: anchoredPgpNodePositions[number_of_nodes][index]
-    });
-  },
-  addNodes = function(graph, nodes, focus, edges) {
-    var sortFuc = nodes.length === 3 ?
-      _.partial(specialSort, getOrderWhenThreeNode(edges)) :
-      simpleSort;
-
+  addNodes = function(graph, nodes, focus) {
     nodes
       .map(_.partial(setFocus, focus))
-      .map(extendIndex)
-      .sort(sortFuc)
-      .map(_.partial(setPosition, nodes.length))
       .map(toNode)
       .forEach(_.partial(addNode, graph));
   },
--- src/js/graph/pgpGraph.js
+++ src/js/graph/pgpGraph.js
@@ -1,25 +1,25 @@
-module.exports = function (pgp) {
-    var graph = require('./lodqaGraph')('lodqa-pgp', {
-        width: 690,
-        height: 100
-    });
+module.exports = function(pgp) {
+  var graph = require('./lodqaGraph')('lodqa-pgp', {
+    width: 690,
+    height: 100
+  });
 
-    graph.addNodes(graph.graph, Object.keys(pgp.nodes).map(function(key) {
-        return {
-            id: key,
-            label: pgp.nodes[key].text
-        };
-    }), pgp.focus, pgp.edges);
+  graph.addNodes(graph.graph, Object.keys(pgp.nodes).map(function(key) {
+    return {
+      id: key,
+      label: pgp.nodes[key].text
+    };
+  }), pgp.focus);
 
-    pgp.edges.forEach(function(edge, index) {
-        graph.addEdge(
-            graph.graph, {
-                id: index,
-                label: edge.text
-            },
-            graph.graph.nodeSet[edge.subject],
-            graph.graph.nodeSet[edge.object],
-            '#000000'
-        );
-    });
+  pgp.edges.forEach(function(edge, index) {
+    graph.addEdge(
+      graph.graph, {
+        id: index,
+        label: edge.text
+      },
+      graph.graph.nodeSet[edge.subject],
+      graph.graph.nodeSet[edge.object],
+      '#000000'
+    );
+  });
 };
