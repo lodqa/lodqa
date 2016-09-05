7de930de81d1326a5a25d88c65baea5f63f730f8
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2016-09-05 16:23:14 +0900

    refactor

--- src/js/loader/loadSolution.js
+++ src/js/loader/loadSolution.js
@@ -1,42 +1,39 @@
-var EventEmitter = require('events').EventEmitter
+const EventEmitter = require('events').EventEmitter
 
-module.exports = function() {
-  var emitter = new EventEmitter,
-    openConnection = function(pathname, config) {
-      var ws = new WebSocket('ws://' + location.host + pathname + '?target=' + config)
+module.exports = class extends EventEmitter {
+  beginSearch(pgp, mappings, pathname, config)  {
+    const ws = openConnection(this, pathname, config)
 
-      ws.onopen = function() {
-        emitter.emit('ws_open')
+    this.once('ws_open', function() {
+      ws.send(JSON.stringify({
+        pgp,
+        mappings
+      }))
+    })
+  }
+}
+
+function openConnection(emitter, pathname, config) {
+  const ws = new WebSocket('ws://' + location.host + pathname + '?target=' + config)
+
+  ws.onopen = function() {
+    emitter.emit('ws_open')
+  }
+  ws.onclose = function() {
+    emitter.emit('ws_close')
+  }
+  ws.onmessage = function(m) {
+    if (m.data === 'start') return
+
+    const jsondata = JSON.parse(m.data);
+
+    ['sparql_count', 'anchored_pgp', 'solution', 'parse_rendering']
+    .forEach(function(event) {
+      if (jsondata.hasOwnProperty(event)) {
+        emitter.emit(event, jsondata[event])
       }
-      ws.onclose = function() {
-        emitter.emit('ws_close')
-      }
-      ws.onmessage = function(m) {
-        if (m.data === 'start') return
+    })
+  }
 
-        var jsondata = JSON.parse(m.data);
-
-        ['sparql_count', 'anchored_pgp', 'solution', 'parse_rendering']
-        .forEach(function(event) {
-          if (jsondata.hasOwnProperty(event)) {
-            emitter.emit(event, jsondata[event])
-          }
-        })
-      }
-
-      return ws
-    }
-
-  return Object.assign(emitter, {
-    beginSearch: function(pgp, mappings, pathname, config) {
-      const ws = openConnection(pathname, config)
-
-      emitter.once('ws_open', function() {
-        ws.send(JSON.stringify({
-          pgp,
-          mappings
-        }))
-      })
-    }
-  })
+  return ws
 }
