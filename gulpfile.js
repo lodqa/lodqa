2d1cfc21f2532a8fb1223ae31ed6c1a426d44ac0
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-25 18:34:47 +0900

    Refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,25 @@
-var gulp = require('gulp'),
-  babel = require('gulp-babel'),
-  browserify = require('gulp-browserify'),
-  src = 'src/js/',
-  dist = 'public/js/'
-
-
-const debug = require('gulp-debug');
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const browserify = require('gulp-browserify')
+const debug = require('gulp-debug')
+const src = 'src/js/'
+const dist = 'public/js/'
 
 gulp
-  .task('browserify', function() {
-    gulp.src(['aa', `${src}lodqa-client.js`, `${src}execute.js`])
-      .pipe(debug({title: 'unicorn:'}))
-      .pipe(browserify({
-        transform: ['babelify']
-      }))
-      .pipe(gulp.dest(dist))
-  })
-  .task('babel', function() {
-    return gulp.src([src + 'index.js', src + 'dashboard.js'])
-      .pipe(babel())
-      .pipe(gulp.dest(dist))
-  })
-  .task('auto_compile', function() {
+  .task('browserify', () => gulp
+    .src([`${src}lodqa-client.js`, `${src}execute.js`])
+    .pipe(debug({title: 'unicorn:'}))
+    .pipe(browserify({
+      transform: ['babelify']
+    }))
+    .pipe(gulp.dest(dist))
+  )
+  .task('babel', () => gulp
+    .src([`${src}index.js`, `${src}dashboard.js`])
+    .pipe(babel())
+    .pipe(gulp.dest(dist))
+  )
+  .task('auto_compile', () => {
     gulp.watch(src + '**', ['browserify'])
     gulp.watch(src + 'index.js', ['babel'])
     gulp.watch(src + 'dashboard.js', ['babel'])
