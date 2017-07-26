7e3a0f5459c345d65eba7ebc089b44efdc35ba73
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-26 15:49:08 +0900

    Show anchored pgp of result

--- src/js/execute.js
+++ src/js/execute.js
@@ -1,13 +1,23 @@
+const Loader = require('./loader/loadSolution')
+const BindResult = require('./controller/bindResult')
+const anchoredPgpTablePresentation = require('./presentation/anchoredPgpTablePresentation')
+
 const pgp = JSON.parse(document.querySelector('#pgp').innerHTML)
 const mappings = JSON.parse(document.querySelector('#mappings').innerHTML)
 const config = document.querySelector('#target').innerHTML
-const ws = new WebSocket(`ws://${location.host}/solutions?target=${config}`)
-ws.onopen = (event) => {
-  event.target.send(JSON.stringify({
-    pgp,
-    mappings
-  }))
-}
-ws.onmessage = (message) => {
-  console.log(message)
-}
+const loader = new Loader()
+const bindResult = new BindResult('lodqa-results')
+
+bindResult.anchoredPgp(loader, anchoredPgpTablePresentation)
+loader.beginSearch(pgp, mappings, '/solutions', config)
+
+// const ws = new WebSocket(`ws://${location.host}/solutions?target=${config}`)
+// ws.onopen = (event) => {
+//   event.target.send(JSON.stringify({
+//     pgp,
+//     mappings
+//   }))
+// }
+// ws.onmessage = (message) => {
+//   console.log(message)
+// }
