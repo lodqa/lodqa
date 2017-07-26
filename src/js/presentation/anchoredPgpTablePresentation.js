6864d1c8b3c07f2d5adbd20b9118396d9594c4dc
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-26 15:46:20 +0900

    Refactor the anchoredPgpTablePresentation

--- src/js/presentation/anchoredPgpTablePresentation.js
+++ src/js/presentation/anchoredPgpTablePresentation.js
@@ -1,45 +1,43 @@
-var _ = require('lodash'),
-  makeTemplate = require('../render/makeTemplate'),
-  template = makeTemplate(function() {
-    /*
-    <div class="result-region anchored_pgp-region">
-        <table class="anchored_pgp-table">
-            <tr>
-                <th></th>
-                <th>head</th>
-                <th>text</th>
-                <th>term</th>
-            </tr>
-            {{#nodes}}
-            <tr class="{{class}}">
-                <td>{{id}}</td>
-                <td>{{head}}</td>
-                <td>{{text}}</td>
-                <td>{{term}}</td>
-            </tr>
-            {{/nodes}}
-        </table>
-    </div>
-    */
-  }),
-  toViewParameters = function(anchored_pgp, node_id) {
-    return Object.assign({}, anchored_pgp.nodes[node_id], {
-      id: node_id,
-      class: node_id === anchored_pgp.focus ? 'focus' : 'normal'
-    })
-  },
-  toArray = require('../collection/toArray')
+const Hogan = require('hogan.js')
+const toArray = require('../collection/toArray')
+
+const template = Hogan.compile(`
+  <div class="result-region anchored_pgp-region">
+      <table class="anchored_pgp-table">
+          <tr>
+              <th></th>
+              <th>head</th>
+              <th>text</th>
+              <th>term</th>
+          </tr>
+          {{#nodes}}
+          <tr class="{{class}}">
+              <td>{{id}}</td>
+              <td>{{head}}</td>
+              <td>{{text}}</td>
+              <td>{{term}}</td>
+          </tr>
+          {{/nodes}}
+      </table>
+  </div>
+`)
 
 module.exports = {
-  onAnchoredPgp: function(domId, anchored_pgp) {
-    var toParams = _.partial(toViewParameters, anchored_pgp),
-      nodes = Object.keys(anchored_pgp.nodes)
-      .map(toParams)
-      .reduce(toArray, []),
-      table = template.render({
-        nodes: nodes
-      })
+  onAnchoredPgp(domId, anchored_pgp) {
+    const nodes = Object.keys(anchored_pgp.nodes)
+      .map((node_id) => toViewParameters(anchored_pgp, node_id))
+      .reduce(toArray, [])
+    const table = template.render({nodes})
+    const element = document.createElement('div')
 
-    $('#' + domId).append(table)
+    element.innerHTML = table
+    document.querySelector(`#${domId}`).appendChild(element.children[0])
   }
+}
+
+function toViewParameters(anchored_pgp, node_id) {
+  return Object.assign({}, anchored_pgp.nodes[node_id], {
+    id: node_id,
+    class: node_id === anchored_pgp.focus ? 'focus' : 'normal'
+  })
 }
--- src/js/render/makeTemplate.js
+++
@@ -1,5 +0,0 @@
-var _ = require('lodash'),
-  multiline = require('multiline'),
-  Hogan = require('hogan.js')
-
-module.exports = _.compose(_.bind(Hogan.compile, Hogan), multiline)
