34d86a2ac2abeefa722fd22984e95e814c884f10
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-26 16:39:26 +0900

    Show the sparql of results

--- src/js/execute.js
+++ src/js/execute.js
@@ -1,6 +1,7 @@
 const Loader = require('./loader/loadSolution')
 const BindResult = require('./controller/bindResult')
 const anchoredPgpTablePresentation = require('./presentation/anchoredPgpTablePresentation')
+const sparqlPresentation = require('./presentation/sparqlPresentation')
 
 const pgp = JSON.parse(document.querySelector('#pgp').innerHTML)
 const mappings = JSON.parse(document.querySelector('#mappings').innerHTML)
@@ -9,6 +10,8 @@ const loader = new Loader()
 const bindResult = new BindResult('lodqa-results')
 
 bindResult.anchoredPgp(loader, anchoredPgpTablePresentation)
+bindResult.sparqlCount(loader, sparqlPresentation)
+bindResult.solution(loader, sparqlPresentation)
 loader.beginSearch(pgp, mappings, '/solutions', config)
 
 // const ws = new WebSocket(`ws://${location.host}/solutions?target=${config}`)
