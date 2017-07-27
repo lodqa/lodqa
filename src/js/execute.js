3e5259119496c610ef2d5b2c942bd9eed6134f61
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-27 14:52:49 +0900

    Show answer lists

--- src/js/execute.js
+++ src/js/execute.js
@@ -2,6 +2,7 @@ const Loader = require('./loader/loadSolution')
 const BindResult = require('./controller/bindResult')
 const anchoredPgpTablePresentation = require('./presentation/anchoredPgpTablePresentation')
 const sparqlPresentation = require('./presentation/sparqlPresentation')
+const answerListPresentation = require('./presentation/answerListPresentation')
 
 const pgp = JSON.parse(document.querySelector('#pgp').innerHTML)
 const mappings = JSON.parse(document.querySelector('#mappings').innerHTML)
@@ -12,6 +13,8 @@ const bindResult = new BindResult('lodqa-results')
 bindResult.anchoredPgp(loader, anchoredPgpTablePresentation)
 bindResult.sparqlCount(loader, sparqlPresentation)
 bindResult.solution(loader, sparqlPresentation)
+bindResult.all(loader, answerListPresentation)
+
 loader.beginSearch(pgp, mappings, '/solutions', config)
 
 // const ws = new WebSocket(`ws://${location.host}/solutions?target=${config}`)
