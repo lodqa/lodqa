b9e0ec9ec76e049f0843fa9af93c96598375d14e
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-27 17:20:43 +0900

    Add the search button

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
-const dashboard = document.querySelector('#dashboard'),
-  results = document.querySelector('#results')
+const dashboard = document.querySelector('#dashboard')
 
 dashboard.addEventListener('click', () => {
   dashboard.classList.remove('dashboard--back')
@@ -9,7 +8,20 @@ dashboard.addEventListener('click', () => {
     .classList.add('examples--hidden')
 })
 
+const results = document.querySelector('#results')
+
 results.addEventListener('click', () => {
   dashboard.classList.add('dashboard--back')
   results.classList.remove('results--back')
+})
+
+const execute_button = document.querySelector('#execute-button')
+
+execute_button.addEventListener('click', (e) => {
+  // Do not submit form.
+  e.preventDefault()
+
+  const form = document.querySelector('#nlqform')
+
+  location.href = `/execute?query=${encodeURIComponent(form.query.value)}&target=${form.target.value}`
 })
