fe08f94eb2f99b037299e2633f26e7ce7d736021
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-31 17:52:42 +0900

    Refactor the fixNodePosition

--- src/js/graph/fixNodePosition.js
+++ src/js/graph/fixNodePosition.js
@@ -1,50 +1,9 @@
-const extendIndex = function(a, index) {
-  a.index = index
-  return a
-}
 const threeNodeOrders = {
   t1: [1, 0, 2],
   t2: [0, 1, 2],
   t3: [0, 2, 1]
 }
-const getNodeOrder = function(id) {
-  return threeNodeOrders[id]
-}
-const getTwoEdgeNode = function(edgeCount) {
-  return Object.keys(edgeCount)
-    .map(function(id) {
-      return {
-        id: id,
-        count: edgeCount[id]
-      }
-    })
-    .filter(function(node) {
-      return node.count === 2
-    })
-    .map(function(node) {
-      return node.id
-    })[0]
-}
-const countEdge = function(edges) {
-  return edges.reduce(function(edgeCount, edge) {
-    edgeCount[edge.subject] ++
-    edgeCount[edge.object] ++
-    return edgeCount
-  }, {
-    t1: 0,
-    t2: 0,
-    t3: 0
-  })
-}
-const getOrderWhenThreeNode = function(edges){
-  getNodeOrder(getTwoEdgeNode(countEdge(edges)))
-}
-const specialSort = function(nodeOrder, a, b) {
-  return nodeOrder[a.index] - nodeOrder[b.index]
-}
-const simpleSort = function(a, b) {
-  return b.index - a.index
-}
+
 const anchoredPgpNodePositions = [
   [],
   [{
@@ -69,14 +28,9 @@ const anchoredPgpNodePositions = [
     y: -40
   }]
 ]
-const setPosition = function(number_of_nodes, term, index) {
-  return Object.assign(term, {
-    position: anchoredPgpNodePositions[number_of_nodes][index]
-  })
-}
 
 module.exports = function(nodes, edges) {
-  var sortFuc = nodes.length === 3 ?
+  const sortFuc = nodes.length === 3 ?
     (a, b) => specialSort(getOrderWhenThreeNode(edges), a, b) :
     simpleSort
 
@@ -84,4 +38,53 @@ module.exports = function(nodes, edges) {
     .map(extendIndex)
     .sort(sortFuc)
     .map((term, index) => setPosition(nodes.length, term, index))
+}
+
+function specialSort(nodeOrder, a, b) {
+  return nodeOrder[a.index] - nodeOrder[b.index]
+}
+
+function getOrderWhenThreeNode(edges){
+  getNodeOrder(getTwoEdgeNode(countEdge(edges)))
+}
+
+function getNodeOrder(id) {
+  return threeNodeOrders[id]
+}
+
+function getTwoEdgeNode(edgeCount) {
+  return Object.keys(edgeCount)
+    .map((id) => ({
+      id,
+      count: edgeCount[id]
+    }))
+    .filter((node) => node.count === 2)
+    .map((node) => node.id)[0]
+}
+
+function countEdge(edges) {
+  return edges.reduce((edgeCount, edge) => {
+    edgeCount[edge.subject] ++
+    edgeCount[edge.object] ++
+    return edgeCount
+  }, {
+    t1: 0,
+    t2: 0,
+    t3: 0
+  })
+}
+
+function simpleSort(a, b) {
+  return b.index - a.index
+}
+
+function extendIndex(a, index) {
+  a.index = index
+  return a
+}
+
+function setPosition(number_of_nodes, term, index) {
+  return Object.assign(term, {
+    position: anchoredPgpNodePositions[number_of_nodes][index]
+  })
 }
