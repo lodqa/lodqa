7b38094b08f9cd807da3498dbda8cea1107a058a
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-08-07 17:21:46 +0900

    Add error handling to check necessary parameters for finding label

--- src/js/presentation/answers-presentation/label-finder/index.js
+++ src/js/presentation/answers-presentation/label-finder/index.js
@@ -55,11 +55,18 @@ function getEndPointInformationFromDom() {
   const endpointUrl = document.querySelector('#endpoint-url')
     .value
   const needProxy = document.querySelector('#need-proxy')
-    .value === 'true'
+    .value
+
+  console.assert(endpointUrl, 'endpointUrl is not set!')
+  console.assert(needProxy, 'needProxy is not set!')
+
+  if (!endpointUrl && !needProxy) {
+    throw new Error('Parameters for finding label is lacked.')
+  }
 
   return {
     endpointUrl,
-    needProxy
+    needProxy: needProxy === 'true'
   }
 }
 
@@ -102,4 +109,5 @@ function fetchLabel(cache, endpointUrl, url, needProxy, callback) {
         cache.setNonResolvableUrl(endpointUrl, url)
       }
     })
+    .catch((error) => console.error(error))
 }
