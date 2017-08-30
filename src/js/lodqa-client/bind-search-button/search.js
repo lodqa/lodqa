9b11ffa44e2ea2c0067d9c5ecb017da00f222262
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-08-30 14:28:39 +0900

    Refactor the bind-search-button

--- src/js/lodqa-client/bind-search-button/search.js
+++ src/js/lodqa-client/bind-search-button/search.js
@@ -1,9 +1,4 @@
-const template = `<h1>Results</h1>
-<div id="lodqa-results"></div>`
-
 module.exports = function(event, loader, pgpElement, mappingsElement) {
-  document.getElementById('results')
-    .innerHTML = template
   event.target.setAttribute('disabled', 'disabled')
 
   const pgp = JSON.parse(pgpElement.innerHTML)
