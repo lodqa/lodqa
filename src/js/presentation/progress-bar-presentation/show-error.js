db62da42bd9e72560759b96863f70184149f4d1e
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-09-12 18:02:00 +0900

    Bug fix: Error icons on the detail-progress-bar is not shown

--- src/js/presentation/progress-bar-presentation/show-error.js
+++ src/js/presentation/progress-bar-presentation/show-error.js
@@ -3,4 +3,6 @@ const getNumberOfAnswers = require('./get-number-of-answers')
 module.exports = function(dom, errorMessage) {
   getNumberOfAnswers(dom)
     .innerHTML = `<i class="fa fa-bomb" aria-hidden="true" title="${errorMessage}"></i>`
+
+  dom.classList.add('progress-bar__detail-progress-bar__sparqls__sparql--error')
 }
