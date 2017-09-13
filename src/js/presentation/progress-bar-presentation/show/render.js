f537dfe49b019c1cd66fa43eeb895c999b8dd575
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-09-13 14:49:59 +0900

    Show 100% at the progress-bar when no sparql

--- src/js/presentation/progress-bar-presentation/show/render.js
+++ src/js/presentation/progress-bar-presentation/show/render.js
@@ -5,8 +5,13 @@ registerPartial()
 
 const template = handlebars.compile(`
   <div class="progress-bar__simple-progress-bar">
-    <progress class="progress-bar__simple-progress-bar__progress" value="0" max="{{sparqls.length}}"></progress>
-    <span class="progress-bar__simple-progress-bar__percentage">0%</span>
+    {{#if sparqls}}
+      <progress class="progress-bar__simple-progress-bar__progress" value="0" max="{{sparqls.length}}"></progress>
+      <span class="progress-bar__simple-progress-bar__percentage">0%</span>
+    {{else}}
+      <progress class="progress-bar__simple-progress-bar__progress"></progress>
+      <span class="progress-bar__simple-progress-bar__percentage">100%</span>
+    {{/if}}
     <span class="progress-bar__simple-progress-bar__show-detail-checkbox">
       <input type="checkbox" id="show-detail-progress-bar">
       <label for="show-detail-progress-bar">Details</label>
