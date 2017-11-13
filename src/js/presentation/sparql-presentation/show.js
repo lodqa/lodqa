9f0cd69a84a2574f74e60c846386f92169903b48
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-11-13 17:39:37 +0900

    Refactor the SparqlPresentation

---
+++ src/js/presentation/sparql-presentation/show.js
@@ -0,0 +1,40 @@
+const createTable = require('./create-table')
+const createDom = require('../create-dom')
+
+module.exports = function(dom, sparqlCount, sparql, sparql_timeout = null) {
+  // Add line break to the sparql
+  const formatedSparql = sparql
+    .replace(/([{])/g, '$1\r\n')
+    .replace(/([}])/g, '\r\n$1')
+    .replace(/(\. )/g, '$1\r\n')
+    .replace(/(, )/g, '$1\r\n')
+    .replace(/( filter)/ig, '\r\n$1')
+    .replace(/( limit)/ig, '\r\n$1')
+
+  // Add a table to the dom tree
+  dom
+    .appendChild(createTable(formatedSparql, sparqlCount))
+
+  // Enable syntax highlight of sparql
+  /*global CodeMirror:true*/
+  const textarea = dom.querySelectorAll('textarea')
+  const editor = CodeMirror.fromTextArea(textarea[textarea.length - 1], {
+    mode: 'application/sparql-query',
+    readOnly: true,
+    lineWrapping: true // wrap line if overflow
+  })
+
+  // Indent all lines
+  const count = editor.lineCount()
+  for (let i = 0; i < count; i++) {
+    editor.indentLine(i)
+  }
+
+  if (sparql_timeout) {
+    dom.appendChild(createDom(`
+      <div class="error-region">
+        <span>This sparql query is timed out!</span>
+      </div>
+    `))
+  }
+}
