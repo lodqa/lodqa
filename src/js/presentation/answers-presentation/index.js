c9533bfb6b1deac24a9c2a8a414e7e98c56fb562
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-11-14 14:26:53 +0900

    Fix the AnswerPresentation that is broken in the search mode

--- src/js/presentation/answers-presentation/index.js
+++ src/js/presentation/answers-presentation/index.js
@@ -4,9 +4,6 @@ module.exports = class {
   constructor(resultDomId, model, findLabelOptions) {
     this.findLabelOptions = findLabelOptions
 
-    // Maybe do no needed
-    model.on('anchored_pgp_reset_event', () => this.setAnchoredPgp(model.anchoredPgp))
-
     model.on('solution_add_event', () => showSolution(document.querySelector(`#${resultDomId}`), model.anchoredPgp, model.currentSolution, findLabelOptions))
   }
 }
