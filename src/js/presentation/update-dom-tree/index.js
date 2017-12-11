5a133f96f91c8a85888300c868d8990f78dbbdfb
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-12-11 16:11:04 +0900

    Add checking input parameters to the update-dom-tree

--- src/js/presentation/update-dom-tree/index.js
+++ src/js/presentation/update-dom-tree/index.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const parse5 = require('parse5')
 const {
   diffAndUpdate,
@@ -10,12 +11,17 @@ module.exports = {
 }
 
 function updateDom(dom, html) {
+  assert(dom, 'dom is a necessary parameter')
+  assert(html.trim(), 'html must have any childNodes')
+
   const [ast] = parse5.parseFragment(html.trim())
     .childNodes
   diffAndUpdate(ast, dom)
 }
 
 function updateChildren(dom, html) {
+  assert(dom, 'dom is a necessary parameter')
+
   const ast = parse5.parseFragment(html.trim())
   diffAndUpdateChildren(ast, dom)
 }
