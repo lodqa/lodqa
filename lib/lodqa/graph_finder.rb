d1234efd9680e0cec21feb6276cca87155e8bf0b
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-12-20 18:06:38 +0900

    Move the GraphFinder under the Lodqa module

--- lib/lodqa/graph_finder.rb
+++ lib/lodqa/graph_finder.rb
@@ -6,301 +6,304 @@ require 'json'
 require 'sparql/client'
 require 'pp'
 require 'lodqa/logger'
+require 'lodqa/sparql_endpoint_timeout_error'
+require 'lodqa/sparql_endpoint_temporary_error'
 
-class GraphFinder
-  # This constructor takes the URL of an end point to be searched
-  # optionally options can be passed to the server of the end point.
-  def initialize(pgp, endpoint, graph_uri, options = {})
-    options ||= {}
-    @debug = options[:debug] || false
+module Lodqa
+  class GraphFinder
+    # This constructor takes the URL of an end point to be searched
+    # optionally options can be passed to the server of the end point.
+    def initialize(pgp, endpoint, graph_uri, options = {})
+      options ||= {}
+      @debug = options[:debug] || false
 
-    @pgp = pgp
-    @endpoint = endpoint
-    @graph_uri = graph_uri
-    @ignore_predicates = options[:ignore_predicates] || []
-    @sortal_predicates = options[:sortal_predicates] || []
-    @max_hop = options[:max_hop] || 2
-  end
+      @pgp = pgp
+      @endpoint = endpoint
+      @graph_uri = graph_uri
+      @ignore_predicates = options[:ignore_predicates] || []
+      @sortal_predicates = options[:sortal_predicates] || []
+      @max_hop = options[:max_hop] || 2
+    end
 
-  # It generates bgps by applying variation operations to the pgp.
-  # The option _max_hop_ specifies the maximum number of hops to be searched.
-  def bgps
-    pgp = @pgp
-    max_hop = @max_hop
+    # It generates bgps by applying variation operations to the pgp.
+    # The option _max_hop_ specifies the maximum number of hops to be searched.
+    def bgps
+      pgp = @pgp
+      max_hop = @max_hop
 
-    Enumerator.new do |y|
-      generate_inverse_variations(
-        generate_split_variations(
-          generate_instantiation_variations(pgp),
-          max_hop
+      Enumerator.new do |y|
+        generate_inverse_variations(
+          generate_split_variations(
+            generate_instantiation_variations(pgp),
+            max_hop
+          )
         )
-      )
-      .each { |bgp| y << bgp }
+        .each { |bgp| y << bgp }
+      end
     end
-  end
 
-  def each_solution
-    bgps.each do |bgp|
-      sparql = compose_sparql(bgp, @pgp)
-      Lodqa::Logger.debug "#{sparql}\n++++++++++"
+    def each_solution
+      bgps.each do |bgp|
+        sparql = compose_sparql(bgp, @pgp)
+        Logger.debug "#{sparql}\n++++++++++"
 
-      begin
-        result = @endpoint.query(sparql)
-      rescue => detail
-        Lodqa::Logger.debug "#{detail}\n=========="
+        begin
+          result = @endpoint.query(sparql)
+        rescue => detail
+          Logger.debug "#{detail}\n=========="
+          sleep(2)
+          next
+        end
+
+        result.each_solution do |solution|
+          yield(solution)
+        end
+
+        Logger.debug "=========="
+
         sleep(2)
-        next
       end
-
-      result.each_solution do |solution|
-        yield(solution)
-      end
-
-      Lodqa::Logger.debug "=========="
-
-      sleep(2)
-    end
-  end
-
-  def each_sparql_and_solution(proc_sparqls, proc_solution, proc_cancel_flag)
-    bgps.each do |bgp|
-      sparql = compose_sparql(bgp, @pgp)
-
-      Lodqa::Logger.debug "#{sparql}\n++++++++++"
-
-      # Send number of spaqls before search
-      proc_sparqls.call sparql
-
-      begin
-        result = @endpoint.query(sparql)
-        solution = {
-          bgp: bgp,
-          sparql: sparql,
-          solutions: result.map{ |s| s.to_h }
-        }
-        proc_solution.call(solution)
-      rescue Lodqa::SparqlEndpointTimeoutError => e
-        Lodqa::Logger.debug 'Sparql Timeout', error_messsage: e.message, trace: e.backtrace
-
-        # Send back error
-        if proc_solution
-          proc_solution.call({bgp: bgp, sparql: sparql, sparql_timeout: {error_message: e}, solutions: []})
-        end
-      rescue Lodqa::SparqlEndpointTemporaryError => e
-        Lodqa::Logger.debug 'Sparql Endpoint Error', error_messsage: e.message, trace: e.backtrace
-
-        # Send back error
-        if proc_solution
-          proc_solution.call({bgp: bgp, sparql: sparql, sparql_timeout: {error_message: e}, solutions: []})
-        end
-      ensure
-        if proc_cancel_flag.call
-          Lodqa::Logger.debug "Stop procedure after a sparql query ends"
-          return
-        end
-      end
-
-      Lodqa::Logger.debug "==========\n"
-
-      # TODO http://rdf.pubannotation.org/sparql requires 2 seconds wait ?
-      # sleep 2
-    end
-  end
-
-  def compose_sparql(bgp, pgp)
-    nodes = pgp[:nodes]
-
-    # get the variables
-    variables = bgp.flatten.uniq - nodes.keys.map(&:to_s)
-
-    # initialize the query
-    query  = "SELECT #{variables.map{|v| '?' + v.to_s}.join(' ')}\n"
-    query += "FROM <#{@graph_uri}>\n"  unless @graph_uri.nil? || @graph_uri.empty?
-    query += "WHERE {"
-
-    # stringify the bgp
-    query += bgp.map{|tp| tp.map{|e| nodes.has_key?(e.to_sym) ? "<#{nodes[e.to_sym][:term]}>" : '?' + e}.join(' ')}.join(' . ') + ' .'
-
-    ## constraints on x-variables (including i-variables)
-    x_variables = variables.dup.keep_if {|v| v[0] == 'x' or v[0] == 'i'}
-
-    # x-variables to be bound to IRIs
-    query += " FILTER (" + x_variables.map{|v| "isIRI(#{'?'+v})"}.join(" && ") + ")" if x_variables.length > 0
-
-    # x-variables to be bound to different IRIs
-    x_variables.combination(2) {|c| query += " FILTER (#{'?'+c[0]} != #{'?'+c[1]})"} if x_variables.length > 1
-
-    ## constraintes on p-variables
-    p_variables = variables.dup.keep_if{|v| v[0] == 'p'}
-
-    # initialize exclude predicates
-    ex_predicates = []
-
-    # filter out ignore predicates
-    ex_predicates += @ignore_predicates
-
-    # filter out sotral predicates
-    ex_predicates += @sortal_predicates
-
-    unless ex_predicates.empty?
-      p_variables.each {|v| query += %| FILTER (str(?#{v}) NOT IN (#{ex_predicates.map{|s| '"'+s+'"'}.join(', ')}))|}
     end
 
-    ## constraintes on s-variables
-    s_variables = variables.dup.keep_if{|v| v[0] == 's'}
-
-    # s-variables to be bound to sortal predicates
-    s_variables.each {|v| query += %| FILTER (str(?#{v}) IN (#{@sortal_predicates.map{|s| '"'+s+'"'}.join(', ')}))|}
-
-    # query += "}"
-    query += "} LIMIT 10"
-  end
-
-  private
-
-  def generate_split_variations(bgps, max_hop = 2)
-    Enumerator.new do |sbgps|
+    def each_sparql_and_solution(proc_sparqls, proc_solution, proc_cancel_flag)
       bgps.each do |bgp|
-        sortal_tps, non_sortal_tps = bgp.partition{|tp| tp[1].start_with? 's'}
-        (1 .. max_hop).to_a.repeated_permutation(non_sortal_tps.length) do |split_scheme|
-          split_tps = generate_split_tps(non_sortal_tps, split_scheme)
-          sbgps << sortal_tps + split_tps
+        sparql = compose_sparql(bgp, @pgp)
+
+        Logger.debug "#{sparql}\n++++++++++"
+
+        # Send number of spaqls before search
+        proc_sparqls.call sparql
+
+        begin
+          result = @endpoint.query(sparql)
+          solution = {
+            bgp: bgp,
+            sparql: sparql,
+            solutions: result.map{ |s| s.to_h }
+          }
+          proc_solution.call(solution)
+        rescue SparqlEndpointTimeoutError => e
+          Logger.debug 'Sparql Timeout', error_messsage: e.message, trace: e.backtrace
+
+          # Send back error
+          if proc_solution
+            proc_solution.call({bgp: bgp, sparql: sparql, sparql_timeout: {error_message: e}, solutions: []})
+          end
+        rescue SparqlEndpointTemporaryError => e
+          Logger.debug 'Sparql Endpoint Error', error_messsage: e.message, trace: e.backtrace
+
+          # Send back error
+          if proc_solution
+            proc_solution.call({bgp: bgp, sparql: sparql, sparql_timeout: {error_message: e}, solutions: []})
+          end
+        ensure
+          if proc_cancel_flag.call
+            Logger.debug "Stop procedure after a sparql query ends"
+            return
+          end
         end
+
+        Logger.debug "==========\n"
+
+        # TODO http://rdf.pubannotation.org/sparql requires 2 seconds wait ?
+        # sleep 2
       end
     end
-  end
 
-  def generate_split_tps(tps, split_scheme)
-    split_tps = []
-    tps.each_with_index do |tp, i|
-      x_variables = (1 ... split_scheme[i]).collect{|j| ("x#{i}#{j}").to_s}
-      p_variables = (1 .. split_scheme[i]).collect{|j| ("p#{i}#{j}").to_s}
+    def compose_sparql(bgp, pgp)
+      nodes = pgp[:nodes]
 
-      # terms including x_variables and the initial and the final terms
-      terms = [tp[0], x_variables, tp[2]].flatten
+      # get the variables
+      variables = bgp.flatten.uniq - nodes.keys.map(&:to_s)
 
-      # triple patterns
-      stps = (0 ... p_variables.length).collect{|j| [terms[j], p_variables[j], terms[j + 1]]}
-      split_tps += stps
-    end
-    split_tps
-  end
+      # initialize the query
+      query  = "SELECT #{variables.map{|v| '?' + v.to_s}.join(' ')}\n"
+      query += "FROM <#{@graph_uri}>\n"  unless @graph_uri.nil? || @graph_uri.empty?
+      query += "WHERE {"
 
-  # make variations by inversing each triple pattern
-  def generate_inverse_variations(bgps)
-    Enumerator.new do |rbgps|
-      bgps.each do |bgp|
-        sortal_tps, non_sortal_tps = bgp.partition{|tp| tp[1].start_with? 's'}
+      # stringify the bgp
+      query += bgp.map{|tp| tp.map{|e| nodes.has_key?(e.to_sym) ? "<#{nodes[e.to_sym][:term]}>" : '?' + e}.join(' ')}.join(' . ') + ' .'
 
-        [false, true].repeated_permutation(non_sortal_tps.length) do |inverse_scheme|
-          rbgps << sortal_tps + non_sortal_tps.map.with_index {|tp, i| inverse_scheme[i]? tp.reverse : tp}
-        end
+      ## constraints on x-variables (including i-variables)
+      x_variables = variables.dup.keep_if {|v| v[0] == 'x' or v[0] == 'i'}
+
+      # x-variables to be bound to IRIs
+      query += " FILTER (" + x_variables.map{|v| "isIRI(#{'?'+v})"}.join(" && ") + ")" if x_variables.length > 0
+
+      # x-variables to be bound to different IRIs
+      x_variables.combination(2) {|c| query += " FILTER (#{'?'+c[0]} != #{'?'+c[1]})"} if x_variables.length > 1
+
+      ## constraintes on p-variables
+      p_variables = variables.dup.keep_if{|v| v[0] == 'p'}
+
+      # initialize exclude predicates
+      ex_predicates = []
+
+      # filter out ignore predicates
+      ex_predicates += @ignore_predicates
+
+      # filter out sotral predicates
+      ex_predicates += @sortal_predicates
+
+      unless ex_predicates.empty?
+        p_variables.each {|v| query += %| FILTER (str(?#{v}) NOT IN (#{ex_predicates.map{|s| '"'+s+'"'}.join(', ')}))|}
       end
-    end
-  end
 
-  # make variations by instantiating terms
-  def generate_instantiation_variations(pgp)
-    return [] if pgp[:edges].empty?
+      ## constraintes on s-variables
+      s_variables = variables.dup.keep_if{|v| v[0] == 's'}
 
-    iids = instantiation_ids pgp
-    bgps = [bgp(pgp)]
+      # s-variables to be bound to sortal predicates
+      s_variables.each {|v| query += %| FILTER (str(?#{v}) IN (#{@sortal_predicates.map{|s| '"'+s+'"'}.join(', ')}))|}
 
-    instantiated_BGPs(iids, bgps, pgp[:focus])
-  end
-
-  def instantiation_ids(pgp)
-    iids = {}
-    pgp[:nodes].each do |id, node|
-      iid = class?(node[:term]) ? 'i' + id.to_s : nil
-      iids[id] = iid unless iid.nil?
+      # query += "}"
+      query += "} LIMIT 10"
     end
 
-    iids
-  end
+    private
 
-  def bgp(pgp)
-    connections = pgp[:edges]
-    connections.map.with_index{|c, i| [c[:subject].to_sym, "p#{i+1}".to_sym, c[:object].to_sym]}
-  end
-
-  def instantiated_BGPs(iids, bgps, focus_id)
-    Enumerator.new do |ibgps|
-      [false, true].repeated_permutation(iids.keys.length) do |instantiate_scheme|
-        # id of the terms to be instantiated
-        itids = iids.keys.keep_if.with_index{|t, i| instantiate_scheme[i]}
-        next unless itids.include?(focus_id.to_sym)
-
-        if bgps.empty? && !itids.empty?
-          ibgp = itids.collect{|t| [iids[t], 's' + t.to_s, t]}
-          ibgps << ibgp
-        else
-          bgps.each do |bgp|
-            # initialize the instantiated bgp with the triple patterns for term instantiation
-            ibgp = itids.collect{|t| [iids[t].to_s, 's' + t.to_s, t.to_s]}
-
-            # add update triples
-            bgp.each{|tp| ibgp << tp.map{|e| itids.include?(e)? iids[e].to_s : e.to_s}}
-
-            ibgps << ibgp
+    def generate_split_variations(bgps, max_hop = 2)
+      Enumerator.new do |sbgps|
+        bgps.each do |bgp|
+          sortal_tps, non_sortal_tps = bgp.partition{|tp| tp[1].start_with? 's'}
+          (1 .. max_hop).to_a.repeated_permutation(non_sortal_tps.length) do |split_scheme|
+            split_tps = generate_split_tps(non_sortal_tps, split_scheme)
+            sbgps << sortal_tps + split_tps
           end
         end
       end
     end
-  end
 
-  def uri?(term)
-    term.start_with? 'http://'
-  end
+    def generate_split_tps(tps, split_scheme)
+      split_tps = []
+      tps.each_with_index do |tp, i|
+        x_variables = (1 ... split_scheme[i]).collect{|j| ("x#{i}#{j}").to_s}
+        p_variables = (1 .. split_scheme[i]).collect{|j| ("p#{i}#{j}").to_s}
 
-  def class?(term)
-    return @endpoint.query(sparql_for(term)).length > 0 if /^http:/.match(term)
+        # terms including x_variables and the initial and the final terms
+        terms = [tp[0], x_variables, tp[2]].flatten
 
-    false
-  end
-
-  def sparql_for(term)
-    sparql  = "SELECT ?p\n"
-    sparql += "FROM <#{@graph_uri}>\n"  unless @graph_uri.nil? || @graph_uri.empty?
-    sparql += "WHERE {?s ?p <#{term}> FILTER (str(?p) IN (#{@sortal_predicates.map{|s| '"'+s+'"'}.join(', ')}))} LIMIT 1"
-    sparql
-  end
-
-
-  def stringify_term(t)
-    if (t.class == RDF::URI)
-      %|<#{t.to_s}>|
-    elsif (t.class == RDF::Literal)
-      if (t.datatype.to_s == "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString")
-        %|"#{t.to_s}"@en|
-      else
-        t.to_s
+        # triple patterns
+        stps = (0 ... p_variables.length).collect{|j| [terms[j], p_variables[j], terms[j + 1]]}
+        split_tps += stps
       end
-    else
-      %|?#{t}|
+      split_tps
+    end
+
+    # make variations by inversing each triple pattern
+    def generate_inverse_variations(bgps)
+      Enumerator.new do |rbgps|
+        bgps.each do |bgp|
+          sortal_tps, non_sortal_tps = bgp.partition{|tp| tp[1].start_with? 's'}
+
+          [false, true].repeated_permutation(non_sortal_tps.length) do |inverse_scheme|
+            rbgps << sortal_tps + non_sortal_tps.map.with_index {|tp, i| inverse_scheme[i]? tp.reverse : tp}
+          end
+        end
+      end
+    end
+
+    # make variations by instantiating terms
+    def generate_instantiation_variations(pgp)
+      return [] if pgp[:edges].empty?
+
+      iids = instantiation_ids pgp
+      bgps = [bgp(pgp)]
+
+      instantiated_BGPs(iids, bgps, pgp[:focus])
+    end
+
+    def instantiation_ids(pgp)
+      iids = {}
+      pgp[:nodes].each do |id, node|
+        iid = class?(node[:term]) ? 'i' + id.to_s : nil
+        iids[id] = iid unless iid.nil?
+      end
+
+      iids
+    end
+
+    def bgp(pgp)
+      connections = pgp[:edges]
+      connections.map.with_index{|c, i| [c[:subject].to_sym, "p#{i+1}".to_sym, c[:object].to_sym]}
+    end
+
+    def instantiated_BGPs(iids, bgps, focus_id)
+      Enumerator.new do |ibgps|
+        [false, true].repeated_permutation(iids.keys.length) do |instantiate_scheme|
+          # id of the terms to be instantiated
+          itids = iids.keys.keep_if.with_index{|t, i| instantiate_scheme[i]}
+          next unless itids.include?(focus_id.to_sym)
+
+          if bgps.empty? && !itids.empty?
+            ibgp = itids.collect{|t| [iids[t], 's' + t.to_s, t]}
+            ibgps << ibgp
+          else
+            bgps.each do |bgp|
+              # initialize the instantiated bgp with the triple patterns for term instantiation
+              ibgp = itids.collect{|t| [iids[t].to_s, 's' + t.to_s, t.to_s]}
+
+              # add update triples
+              bgp.each{|tp| ibgp << tp.map{|e| itids.include?(e)? iids[e].to_s : e.to_s}}
+
+              ibgps << ibgp
+            end
+          end
+        end
+      end
+    end
+
+    def uri?(term)
+      term.start_with? 'http://'
+    end
+
+    def class?(term)
+      return @endpoint.query(sparql_for(term)).length > 0 if /^http:/.match(term)
+
+      false
+    end
+
+    def sparql_for(term)
+      sparql  = "SELECT ?p\n"
+      sparql += "FROM <#{@graph_uri}>\n"  unless @graph_uri.nil? || @graph_uri.empty?
+      sparql += "WHERE {?s ?p <#{term}> FILTER (str(?p) IN (#{@sortal_predicates.map{|s| '"'+s+'"'}.join(', ')}))} LIMIT 1"
+      sparql
+    end
+
+
+    def stringify_term(t)
+      if (t.class == RDF::URI)
+        %|<#{t.to_s}>|
+      elsif (t.class == RDF::Literal)
+        if (t.datatype.to_s == "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString")
+          %|"#{t.to_s}"@en|
+        else
+          t.to_s
+        end
+      else
+        %|?#{t}|
+      end
+    end
+
+    # def stringify_term (t)
+    #   if (t.class == RDF::URI)
+    #     %|<#{t.to_s}>|
+    #   elsif (t.class == RDF::Literal)
+    #     if (t.datatype.to_s == "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString")
+    #       %|"#{t.to_s}"@en|
+    #     else
+    #       t.to_s
+    #     end
+    #   else
+    #     %|?#{t}|
+    #   end
+    # end
+
+    # the sparql-client gem does not support FILTER pattern
+    def _compose_sparql(x_variables, p_variables, bgp)
+      query = @endpoint.select(*p_variables, *x_variables).where(*bgp).limit(10)
+      query.to_s
     end
   end
-
-  # def stringify_term (t)
-  #   if (t.class == RDF::URI)
-  #     %|<#{t.to_s}>|
-  #   elsif (t.class == RDF::Literal)
-  #     if (t.datatype.to_s == "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString")
-  #       %|"#{t.to_s}"@en|
-  #     else
-  #       t.to_s
-  #     end
-  #   else
-  #     %|?#{t}|
-  #   end
-  # end
-
-  # the sparql-client gem does not support FILTER pattern
-  def _compose_sparql(x_variables, p_variables, bgp)
-    query = @endpoint.select(*p_variables, *x_variables).where(*bgp).limit(10)
-    query.to_s
-  end
-
 end
 
 if __FILE__ == $0
