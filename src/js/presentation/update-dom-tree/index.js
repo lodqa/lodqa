37542025674a1b18311f0a6b4c838f88b46bda95
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-03-13 14:39:24 +0900

    Add checking length of input html string to the update-dom-tree

--- src/js/presentation/update-dom-tree/index.js
+++ src/js/presentation/update-dom-tree/index.js
@@ -4,6 +4,11 @@ const diffAndUpdateChildren = require('./diff-and-update-children')
 
 module.exports = function updateChildren(dom, html) {
   assert(dom, 'dom is a necessary parameter')
+  assert(html.length < 1000000, 'stop rendering because html length is more than one million.')
+
+  if (html.length > 100000) {
+    console.warn(`html is too long: ${html.replace(/[\n\r]+/g, '').replace(/\s{2,10}/g, ' ').substr(0, 100)}`)
+  }
 
   const ast = parse5.parseFragment(html.trim())
   diffAndUpdateChildren(ast, dom)
