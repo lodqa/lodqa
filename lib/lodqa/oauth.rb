64c225f882fca0326a28b8a16e36ee63b9e2f3a5
sharsukhchuluun <sukhchuluun@luxiar.com>
2020-02-04 13:53:05 +0900

    issue #12 コメンt記載

--- lib/lodqa/oauth.rb
+++ lib/lodqa/oauth.rb
@@ -16,6 +16,9 @@ module Lodqa
 
     private
 
+    # アクセストークン取得
+    #   ユーザーがアプリケーションにアクセス権を付与済みであれば、更新トークンとアクセストークンの取得した承認コードを交換する。
+    #   参考URL（https://developers.google.com/youtube/v3/guides/auth/server-side-web-apps?hl=ja）
     def access_token_id
       uri = URI.parse("#{ENV['URL_TOKEN']}")
       request = Net::HTTP::Post.new(uri)
@@ -30,6 +33,13 @@ module Lodqa
       response = Net::HTTP.start(uri.hostname, uri.port, options(uri)) do |http|
         http.request(request)
       end
+      # レスポンス情報の例：
+      #   {
+      #     "access_token" : "ya29.AHES6ZTtm7SuokEB-RGtbBty9IIlNiP9-eNMMQKtXdMP3sfjL1Fc",
+      #     "token_type" : "Bearer",
+      #     "expires_in" : 3600,
+      #     "refresh_token" : "1/HKSmLFXzqP0leUihZp2xUt3-5wkU7Gmu2Os_eBnzw74"
+      #   }
 
       case response.code
       when '200' then
@@ -41,6 +51,8 @@ module Lodqa
       end
     end
 
+    # emailアドレス取得
+    #   参考URL（https://developers.google.com/identity/sign-in/web/backend-auth#calling-the-tokeninfo-endpoint）
     def token_info_email token_id
       return nil unless token_id
 
