068d9acb912d6e2e59c445dc82860b529c2d24e0
sharsukhchuluun <sukhchuluun@luxiar.com>
2020-03-24 18:33:10 +0900

    issue #39 require移動

--- lodqa-ws.rb
+++ lodqa-ws.rb
@@ -16,7 +16,6 @@ require 'lodqa/source_channel'
 require 'lodqa/sparqls_count'
 require 'lodqa/oauth'
 require 'lodqa/configuration'
-require 'enju_access/cgi_accessor'
 require 'lodqa/sources'
 require 'lodqa/pgp_factory'
 
