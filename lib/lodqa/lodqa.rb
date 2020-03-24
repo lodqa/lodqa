07718e95d3e4a7d864f12e1e91e00cca1f5afdbf
sharsukhchuluun <sukhchuluun@luxiar.com>
2020-03-24 13:15:31 +0900

    issue #39 move source

--- lib/lodqa/lodqa.rb
+++ lib/lodqa/lodqa.rb
@@ -9,6 +9,7 @@ require 'logger/logger'
 require 'lodqa/sources'
 require 'lodqa/pgp_factory'
 require 'lodqa/configuration'
+require 'lodqa/bs_client'
 
 module Lodqa
   class Lodqa
@@ -29,83 +30,11 @@ module Lodqa
 
       Enumerator.new do |y|
         begin
-          anchored_pgps.each do |anchored_pgp|
-            to_sparql(anchored_pgp){ |sparql| y << sparql}
-          end
+          BSClient.sparqls @pgp, @mappings, @graph_finder, y
         rescue SparqlClient::EndpointError => e
           Logger::Logger.debug "The SPARQL Endpoint #{e.endpoint_name} has a persistent error, continue to the next Endpoint", error_message: e.message
         end
       end
-    end
-
-    def anchored_pgps
-      Logger::Logger.debug "start #{self.class.name}##{__method__}"
-
-      Enumerator.new do |anchored_pgps|
-        @pgp[:nodes].delete_if{|n| nodes_to_delete.include? n}
-        @pgp[:edges].uniq!
-        terms = @pgp[:nodes].values.map{|n| @mappings[n[:text].to_sym]}
-
-        terms.map!{|t| t.nil? ? [] : t}
-
-        Logger::Logger.debug "terms: #{ terms.first.product(*terms.drop(1)) }"
-
-        terms.first.product(*terms.drop(1))
-          .each do |ts|
-            anchored_pgp = pgp.dup
-            anchored_pgp[:nodes] = pgp[:nodes].dup
-            anchored_pgp[:nodes].each_key{|k| anchored_pgp[:nodes][k] = pgp[:nodes][k].dup}
-            anchored_pgp[:nodes].each_value.with_index{|n, i| n[:term] = ts[i]}
-
-            anchored_pgps << anchored_pgp
-          end
-      end
-    end
-
-    private
-
-    def to_sparql(anchored_pgp)
-      if @cancel_flag
-        Logger::Logger.debug "Stop during creating SPARQLs for anchored_pgp: #{anchored_pgp}"
-        return
-      end
-
-      Logger::Logger.debug "create graph finder"
-
-      if @cancel_flag
-        Logger::Logger.debug "Stop during creating SPARQLs for anchored_pgp: #{anchored_pgp}"
-        return
-      end
-
-      Logger::Logger.debug "return SPARQLs of bgps"
-      @graph_finder.sparqls_of(anchored_pgp) do |bgp, sparql|
-        yield sparql
-      end
-    end
-
-    def nodes_to_delete
-      Logger::Logger.debug "start #{self.class.name}##{__method__}"
-
-      nodes_to_delete = []
-      @pgp[:nodes].each_key do |n|
-        if @mappings[@pgp[:nodes][n][:text]].nil? || @mappings[@pgp[:nodes][n][:text]].empty?
-          connected_nodes = []
-          @pgp[:edges].each{|e| connected_nodes << e[:object] if e[:subject] == n}
-          @pgp[:edges].each{|e| connected_nodes << e[:subject] if e[:object] == n}
-
-          # if it is a passing node
-          if connected_nodes.length == 2
-            nodes_to_delete << n
-            @pgp[:edges].each do |e|
-              e[:object]  = connected_nodes[1] if e[:subject] == connected_nodes[0] && e[:object]  == n
-              e[:subject] = connected_nodes[1] if e[:subject] == n && e[:object]  == connected_nodes[0]
-              e[:object]  = connected_nodes[0] if e[:subject] == connected_nodes[1] && e[:object]  == n
-              e[:subject] = connected_nodes[0] if e[:subject] == n && e[:object]  == connected_nodes[1]
-            end
-          end
-        end
-      end
-      nodes_to_delete
     end
   end
 end
