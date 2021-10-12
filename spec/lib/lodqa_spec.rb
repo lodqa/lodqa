cacaa54202020b360a1fb17fd4fe43e8a4eb7470
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-10-12 16:03:45 +0900

    refactor: Delete comment outed tests

--- spec/lib/lodqa_spec.rb
+++ spec/lib/lodqa_spec.rb
@@ -18,34 +18,3 @@ describe "LODQA", "error-handling on configuration" do
   it "should be able to handle a missing config file"
 end
 
-# describe "LODQA", "error-handling in get_texps method" do
-#   it "should handle case where there is no content for the texps variable" 
-# end
-
-# describe "LODQA", "error-handling in get_sparql method" do
-#   it "should handle null vid"
-#   it "should handle empty vid"
-#   it "should handle invalid vid"
-#   it "should handle null acronym"
-#   it "should handle empty acronym"
-# end
-
-# describe "LODQA", "error-handling in find_term_uris method" do
-#   it "should handle null vid"
-#   it "should handle empty vid"
-#   it "should handle invalid vid"
-# end
-
-# describe "LODQA", "error-handling when using the command line" do
-#   it "should handle a missing semantic types file"
-# end
-
-# describe "LODQA", "basic functionality testing" do
-#   it "should handle good inputs when initialized"
-#   it "should handle valid queries to parser"
-#   it "should be able to look up existent terms given a good vid"
-#   it "should be able to take valid arguments from the command line"
-#   it "should be able to handle empty argument list from the command line"
-#   it "should handle a query that contains the word 'me'"
-#   it "should handle a query that does not contain the word 'me'" 
-# end
